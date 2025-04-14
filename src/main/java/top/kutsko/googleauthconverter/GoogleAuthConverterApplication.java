b5c4792f39a515fe2067c59eedf0a27043072659
Vasili Kutsko <vasiliy.kutsko@hesfintech.com>
2025-04-14 23:04:05 +0300

    init

---
+++ src/main/java/top/kutsko/googleauthconverter/GoogleAuthConverterApplication.java
@@ -0,0 +1,13 @@
+package top.kutsko.googleauthconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleAuthConverterApplication.class, args);
+    }
+
+}
